77f58374a4d83cc4fc27220ed8c68da77af1320c
Facundo Pedrazzini <facundo.pedrazzini@proton.ch>
2026-02-17 12:42:57 +0100

    MEET-850: Upselling modal unification

--- applications/meet/src/app/components/TranslucentModal/TranslucentModal.tsx
+++ applications/meet/src/app/components/TranslucentModal/TranslucentModal.tsx
@@ -16,12 +16,12 @@ export const TranslucentModal = ({
     children: React.ReactNode;
 }) => (
     <ModalTwo open={open} onClose={onClose} rootClassName="translucent-modal" size="full" fullscreen>
-        <div className="w-full meet-container-padding-x overflow-y-auto h-full">
+        <div className="w-full meet-container-padding-x overflow-y-auto h-full flex flex-column">
             <div className="flex justify-space-between items-center pt-5 pb-5">
                 <img src={brand} alt={MEET_APP_NAME} className="h-custom" style={{ '--h-custom': '2.5rem' }} />
                 <CloseButton onClose={onClose} />
             </div>
-            <div className="translucent-modal_container flex">
+            <div className="translucent-modal_container flex flex-1">
                 <div className="m-auto pt-10 pb-10">{children}</div>
             </div>
         </div>
