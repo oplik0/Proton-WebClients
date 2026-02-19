4508c05934673c40c17bba12fd3ee9ab2e30a96d
Dominik Jankovic <dominik.jankovic@proton.ch>
2026-02-19 17:29:55 +0000

    Fix Dashboard showing OLES card title when card is conditionally unrendered

--- packages/activation/src/oles/components/DashboardGuide/DashboardGuide.tsx
+++ packages/activation/src/oles/components/DashboardGuide/DashboardGuide.tsx
@@ -1,6 +1,8 @@
 import type { FC } from 'react';
 import type React from 'react';
 
+import { c } from 'ttag';
+
 import SkeletonLoader from '@proton/components/components/skeletonLoader/SkeletonLoader';
 import { IcCheckmark } from '@proton/icons/icons/IcCheckmark';
 import clsx from '@proton/utils/clsx';
@@ -21,35 +23,42 @@ const DashboardGuide: FC = () => {
     }
 
     return (
-        <ol className="unstyled border rounded">
-            {steps.map((step, ix) => (
-                <li
-                    key={step.id}
-                    className={clsx(
-                        ix !== steps.length - 1 && 'border-bottom',
-                        'px-4 py-3 items-center justify-space-between flex'
-                    )}
-                >
-                    <div className="flex flex-nowrap items-center">
-                        <div
-                            className={clsx(
-                                step.completed ? 'bg-primary color-white' : 'color-primary',
-                                'border border-primary rounded-full mr-2 ratio-square w-custom text-xs flex items-center justify-center'
-                            )}
-                            style={{ '--w-custom': '1rem' }}
-                        >
-                            {step.completed ? <IcCheckmark /> : <span>{ix + 1}</span>}
+        <>
+            <h2>{c('BOSS').t`Finish setting up your business account`}</h2>
+            <ol className="unstyled border rounded">
+                {steps.map((step, ix) => (
+                    <li
+                        key={step.id}
+                        className={clsx(
+                            ix !== steps.length - 1 && 'border-bottom',
+                            'px-4 py-3 items-center justify-space-between flex'
+                        )}
+                    >
+                        <div className="flex flex-nowrap items-center">
+                            <div
+                                className={clsx(
+                                    step.completed ? 'bg-primary color-white' : 'color-primary',
+                                    'border border-primary rounded-full mr-2 ratio-square w-custom text-xs flex items-center justify-center'
+                                )}
+                                style={{ '--w-custom': '1rem' }}
+                            >
+                                {step.completed ? <IcCheckmark /> : <span>{ix + 1}</span>}
+                            </div>
+                            <div className={clsx({ 'text-strike': step.completed })}>{step.text}</div>
                         </div>
-                        <div className={clsx({ 'text-strike': step.completed })}>{step.text}</div>
-                    </div>
-                    {step.action && (
-                        <step.action
-                            className={clsx(step.completed && 'visibility-hidden', 'text-semibold', 'color-primary')}
-                        />
-                    )}
-                </li>
-            ))}
-        </ol>
+                        {step.action && (
+                            <step.action
+                                className={clsx(
+                                    step.completed && 'visibility-hidden',
+                                    'text-semibold',
+                                    'color-primary'
+                                )}
+                            />
+                        )}
+                    </li>
+                ))}
+            </ol>
+        </>
     );
 };
 
