fe770717ee83099e86167bc9d998ddf9aa22ef14
Vijayasingam Thanasekaran <vijayasingam.thanasekaran@proton.ch>
2026-02-19 15:33:21 +0000

    Delete unused code

--- packages/shared/lib/api/memberSessions.ts
+++ packages/shared/lib/api/memberSessions.ts
@@ -1,19 +1,3 @@
-export const createSession = (memberID: string, data: any) => ({
-    method: 'post',
-    url: `members/${memberID}/sessions`,
-    data,
-});
-
-export const getSessions = (memberID: string) => ({
-    method: 'get',
-    url: `members/${memberID}/sessions`,
-});
-
-export const revokeSession = (memberID: string, uid: string) => ({
-    method: 'delete',
-    url: `members/${memberID}/sessions/${uid}`,
-});
-
 export const revokeSessions = (memberID: string) => ({
     method: 'delete',
     url: `members/${memberID}/sessions`,
